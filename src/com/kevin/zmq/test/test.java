d62fb0a06f7fe0413244820c0897e0a5bbbe3de0
公共 <@zmq>
2018-05-12 10:47:21 +0800

    阶乘求解

--- src/com/kevin/zmq/test/test.java
+++ src/com/kevin/zmq/test/test.java
@@ -1,10 +1,24 @@
 package com.kevin.zmq.test;
 
+import org.junit.Test;
+
 public class test {
 
 	public static void main(String[] args) {
 		System.out.println("你好编程世界，你是否还记得最初的自己");
-
+        
 	}
-
+	/**
+	 * 求5！
+	 */
+	@Test
+    public void test(){
+    	int i=0;
+    	int f=1;
+    	for(i=1;i<=8;i++){
+    		f=f*i;
+    		System.out.println(f);
+    	}
+    	System.out.println(f);
+    }
 }
